7224d818487215ecb951ed0eab16e68730e4ab32
MathiasVeraM <mathias.vera@udla.edu.ec>
2024-12-03 08:48:35 -0500

    Se implementaron las formas para guardar los datos y extraerlos de archivos

--- VeraMathiasExamenP2/Repositories/RecargasRepositorio.cs
+++ VeraMathiasExamenP2/Repositories/RecargasRepositorio.cs
@@ -1,7 +1,9 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 using VeraMathiasExamenP2.Models;
 
@@ -9,6 +11,7 @@ namespace VeraMathiasExamenP2.Repositories
 {
     public class RecargasRepositorio
     {
+        public string _filename = Path.Combine(FileSystem.AppDataDirectory, "MathiasVera.txt");
         public VeraMathiasRecarga DevuelveInfoRecarga()
         {
             return new VeraMathiasRecarga()
@@ -17,8 +20,23 @@ namespace VeraMathiasExamenP2.Repositories
                 Numero=0979386716,
                 Nombre="Mathias"
             };
+        }
 
-            
+        public void GuardarEnArchivo(VeraMathiasRecarga veraMathiasRecarga)
+        {
+            string json = JsonConvert.SerializeObject(veraMathiasRecarga);
+            File.WriteAllText(_filename, json);
+        }
+
+        public VeraMathiasRecarga ObtenerInformacionRecarga()
+        {
+            VeraMathiasRecarga veraMathiasRecarga = new VeraMathiasRecarga();
+            if (File.Exists(_filename))
+            {
+                string data = File.ReadAllText(_filename);
+                veraMathiasRecarga = JsonConvert.DeserializeObject<VeraMathiasRecarga>(data);
+            }
+            return veraMathiasRecarga;
         }
     }
 }
