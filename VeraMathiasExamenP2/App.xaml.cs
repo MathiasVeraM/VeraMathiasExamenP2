73137a74f3e37348b2796126cee6ba99065a5647
MathiasVeraM <mathias.vera@udla.edu.ec>
2024-12-03 07:21:46 -0500

    Agregar archivos de proyecto.

---
+++ VeraMathiasExamenP2/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace VeraMathiasExamenP2
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
